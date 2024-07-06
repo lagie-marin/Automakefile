e16a80fb0d38187d03dbe0f9c92532c6381511cb
lagie-marin <marin.lagie@epitech.eu>
2024-07-06 11:28:58 +0200

    [ADD]project Automakefile

---
+++ fakeProj1/include/calc/calc.h
@@ -0,0 +1,9 @@
+/*
+** EPITECH PROJECT, 2024
+** B-SYN-200-LYN-2-1-automakefile-marin.lagie
+** File description:
+** calc.h
+*/
+#ifndef CALC_H
+    #define CALC_H
+#endif
---
+++ fakeProj1/include/fakeProj.h
@@ -0,0 +1,9 @@
+/*
+** EPITECH PROJECT, 2024
+** B-SYN-200-LYN-2-1-automakefile-marin.lagie
+** File description:
+** fakeProj.h
+*/
+#ifndef FAKEPROJ_H
+    #define FAKEPROJ_H
+#endif
